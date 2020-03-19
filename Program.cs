e8268c21cc2f50280fa716aa2c0ddd168580fa1b
Riley <tudbury@gmail.com>
2020-03-19 16:20:12 -0600

    working

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace cs_day1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Boolean running = true;
+      int playerScore = 0;
+      int compScore = 0;
+      Console.Write("Please enter your name: ");
+      string name = Console.ReadLine();
+      Console.WriteLine($"Thank you, {name}");
+
+      Console.WriteLine("Would you like to play a game? (y/n)");
+      if (Console.ReadKey().KeyChar == 'n' || Console.ReadKey().KeyChar == 'N')
+      {
+        running = false;
+      }
+      while (running)
+      {
+
+        Console.Clear();
+        Console.WriteLine("Rock, Paper, or Scissors?");
+        string playerChoice = Console.ReadLine().ToLower().Trim();
+        Random rnd = new Random();
+        int compRandom = rnd.Next(3);
+        string[] choices = new string[]{
+            "rock",
+            "paper",
+            "scissors"
+        };
+
+        string compChoice = choices[compRandom];
+        Console.WriteLine($"Player chose: {playerChoice}, Computer chose: {compChoice}");
+        if (playerChoice == compChoice)
+        {
+          Console.WriteLine("It's a tie!");
+        }
+        else if (playerChoice == "rock")
+        {
+          if (compChoice == "paper")
+          {
+            Console.WriteLine("Computer wins!");
+            compScore++;
+          }
+          else
+          {
+            Console.WriteLine($"{name} wins!");
+            playerScore++;
+          }
+        }
+        else if (playerChoice == "paper")
+        {
+          if (compChoice == "scissors")
+          {
+            Console.WriteLine("Computer wins!");
+            compScore++;
+          }
+          else
+          {
+            Console.WriteLine($"{name} wins!");
+            playerScore++;
+          }
+        }
+        else if (playerChoice == "scissors")
+        {
+          if (compChoice == "rock")
+          {
+            Console.WriteLine("Computer wins!");
+            compScore++;
+          }
+          else
+          {
+            Console.WriteLine($"{name} wins!");
+            playerScore++;
+          }
+        }
+        Console.WriteLine($"Current Score: {name}: {playerScore} Computer: {compScore}");
+        Console.WriteLine("Would you like to play again? (y/n)");
+        if (Console.ReadKey().KeyChar == 'n' || Console.ReadKey().KeyChar == 'N')
+        {
+          running = false;
+          Console.WriteLine();
+          Console.WriteLine($"Thank you for playing, {name}!");
+          break;
+        }
+      }
+    }
+  }
+}
